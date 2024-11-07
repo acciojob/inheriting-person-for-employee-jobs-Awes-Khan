5a94a07240cdd82fc5a05321aadbc71f27469dc8
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-11-07 22:09:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ class Person{
 		this.age = age;
 	}
 	 greet(){
-		 // console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-		 console.log("Hello, my name is " + this.name +", I am "+this.age+" years old.");
+		// console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+		  console.log("Hello, my name is " + this.name +", I am "+this.age+" years old.");
 	 }
 }
 
@@ -16,12 +16,11 @@ class Employee extends Person {
 		this.jobTitle = jobTitle;
 	}
 	jobGreet(){
-	  // console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+	 // console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 	  console.log("Hello, my name is "+this.name+", I am "+this.age+" years old, and my job title is "+this.jobTitle+".");
 	}
 	
 }
-
 
 // Do not change code below this line
 window.Person = Person;
